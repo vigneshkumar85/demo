648786b6bde625d665d745d5d0125c8265b75cca
Vignesh Kumar <vigneshkumar85@gmail.com>
2021-08-18 18:01:19 -0700

    first commit

---
+++ src/main/java/com/example/demo/Hellocontroller.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hellocontroller {
+
+    @GetMapping("/getDetails")
+	public String index() {
+		return "Welcome to Sample Vignesh Project!!!";
+	}
+    
+}
