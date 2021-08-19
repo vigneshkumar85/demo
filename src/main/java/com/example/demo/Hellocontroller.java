782e65ede7118923295ca8a9d04afe6d85ac23ba
Vignesh Kumar <vigneshkumar85@gmail.com>
2021-08-18 21:55:33 -0700

    Commit_100

--- src/main/java/com/example/demo/Hellocontroller.java
+++ src/main/java/com/example/demo/Hellocontroller.java
@@ -8,7 +8,7 @@ public class Hellocontroller {
 
     @GetMapping("/")
 	public String index() {
-		return "Welcome to Sample Vikki11 Project!!!";
+		return "Welcome to Sample Vikki22 Project!!!";
 	}
     
 }
